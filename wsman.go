8ee42db6966b1dfc43d2891d9850e55c4b66891f
Joko Sastriawan <joko.sastriawan@gmail.com>
2022-04-09 13:16:26 -0700

    Enhance parser and pull to get multiple instances Add Put action support

--- wsman.go
+++ wsman.go
@@ -3,6 +3,7 @@ package gowsman
 import (
 	"fmt"
 	"io"
+	"reflect"
 	"strconv"
 
 	"github.com/beevik/etree"
@@ -65,7 +66,28 @@ var wsmanPullTemplate = `
 	<a:Body>
 		<Pull xmlns="http://schemas.xmlsoap.org/ws/2004/09/enumeration">
 			<EnumerationContext>%s</EnumerationContext>
+			<MaxElements>999</MaxElements>
 		</Pull>
+	</a:Body>
+</a:Envelope>
+`
+
+var wsmanPutTemplate = `
+<?xml version="1.0" encoding="UTF-8" ?>
+<a:Envelope xmlns:a="http://www.w3.org/2003/05/soap-envelope"
+	xmlns:b="http://schemas.xmlsoap.org/ws/2004/08/addressing" 
+	xmlns:c="http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd"> 
+	<a:Header>
+		<b:Action mustUnderstand="true">http://schemas.xmlsoap.org/ws/2004/09/transfer/Put</b:Action>
+		<b:To>/wsman</b:To>
+		<c:ResourceURI>%s</c:ResourceURI>
+		<b:MessageID>%s</b:MessageID>
+		<b:ReplyTo><b:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</b:Address></b:ReplyTo>
+		<c:OperationTimeout>PT60S</c:OperationTimeout>
+		%s
+	</a:Header>
+	<a:Body>
+%s
 	</a:Body>
 </a:Envelope>
 `
@@ -81,26 +103,78 @@ func newWSMan() *WSMan {
 }
 
 func parseChildElements(el *etree.Element) map[string]interface{} {
+	// proper node structure
 	var node = map[string]interface{}{}
+	// track every child element as array to detect potential array
+	var node_array = make(map[string][]interface{})
+
 	for _, v := range el.ChildElements() {
 		if len(v.ChildElements()) > 0 {
 			var node1 = parseChildElements(v)
-			node[v.Tag] = node1
+			if node_array[v.Tag] == nil {
+				var chld_array = []interface{}{node1}
+				node_array[v.Tag] = chld_array
+			} else {
+				node_array[v.Tag] = append(node_array[v.Tag], node1)
+			}
 		} else {
 			val_int, err := strconv.ParseInt(v.Text(), 10, 64)
 			if err == nil {
-				node[v.Tag] = val_int
+				var chld_array = []interface{}{val_int}
+				node_array[v.Tag] = chld_array
 				continue
 			}
 			val_bool, err := strconv.ParseBool(v.Text())
 			if err == nil {
-				node[v.Tag] = val_bool
+				var chld_array = []interface{}{val_bool}
+				node_array[v.Tag] = chld_array
 				continue
 			}
-			node[v.Tag] = v.Text()
+			var chld_array = []interface{}{v.Text()}
+			node_array[v.Tag] = chld_array
+		}
+	}
+	for k, v := range node_array {
+		if len(v) > 1 {
+			node[k] = v
+		} else {
+			node[k] = v[0]
 		}
 	}
 	return node
+}
+
+func createPutStringFromBody(objname string, body map[string]interface{}) string {
+	if body == nil {
+		return ""
+	}
+	pfx := objname[0:3]
+	val, ok := ns_prefix[pfx]
+	resuri := ""
+	if ok {
+		resuri = val + objname
+	} else {
+		return ""
+	}
+	var result = "\t\t<r:" + objname + " xmlns:r=\"" + resuri + "\">\n"
+	obj := body[objname]
+	for k, v := range obj.(map[string]interface{}) {
+		if _, ok := v.(string); ok {
+			result += "\t\t\t<r:" + k + ">" + v.(string) + "</r:" + k + ">\n"
+		} else if _, ok = v.(int); ok {
+			result += "\t\t\t<r:" + k + ">" + strconv.Itoa(v.(int)) + "</r:" + k + ">\n"
+		} else if _, ok = v.(int64); ok {
+			result += "\t\t\t<r:" + k + ">" + strconv.FormatInt(v.(int64), 10) + "</r:" + k + ">\n"
+		} else if _, ok = v.(bool); ok {
+			result += "\t\t\t<r:" + k + ">" + strconv.FormatBool(v.(bool)) + "</r:" + k + ">\n"
+		} else if v == nil {
+			result += "\t\t\t<r:" + k + "></r:" + k + ">\n"
+		} else {
+			fmt.Println(k + ": " + reflect.TypeOf(v).Name())
+		}
+	}
+	result += "\t\t</r:" + objname + ">"
+	return result
 }
 
 func (wsman *WSMan) ParseWsman(reader io.Reader) (*WSMan, error) {
@@ -146,6 +220,17 @@ func (wsman WSMan) CreateWsmanPull(obj string, message_id string, enum_ctx strin
 	if ok {
 		n_obj := val + obj
 		wsman_str = fmt.Sprintf(wsmanPullTemplate, n_obj, message_id, enum_ctx)
+	}
+	return wsman_str
+}
+
+func (wsman WSMan) CreateWsmanPut(obj string, message_id string, selector string, body string) string {
+	wsman_str := ""
+	pfx := obj[0:3]
+	val, ok := ns_prefix[pfx]
+	if ok {
+		n_obj := val + obj
+		wsman_str = fmt.Sprintf(wsmanPutTemplate, n_obj, message_id, selector, body)
 	}
 	return wsman_str
 }
--- wsman_test.go
+++ wsman_test.go
@@ -108,8 +108,69 @@ func TestEnumPullCIMSoftwareIdentity(t *testing.T) {
 		}
 		items := pull_resp.(map[string]interface{})["Items"]
 		enum_result = append(enum_result, items.(map[string]interface{})["CIM_SoftwareIdentity"])
+		if _, ok := pull_resp.(map[string]interface{})["EndOfSequence"]; ok {
+			t.Logf("Found end of sequence, do not pull again")
+			break
+		}
 	}
 	js, _ := json.MarshalIndent(enum_result, "", "   ")
 	t.Logf("%s\n", string(js))
+}
 
+func TestCreatePuWsmanBody(t *testing.T) {
+	wsman := new(WSMan)
+	f, err := os.Open("test/data/gensettings.xml")
+	if err != nil {
+		t.Fail()
+	}
+	ws, err := wsman.ParseWsman(bufio.NewReader(f))
+	str := createPutStringFromBody("AMT_GeneralSettings", ws.Body)
+	t.Logf("\n%s\n", str)
+}
+
+func TestUpdateAMTGeneralSettings(t *testing.T) {
+	cfg_bytes, err := ioutil.ReadFile("test/data/cred.json")
+	if err != nil {
+		t.Logf(err.Error())
+		t.Fail()
+	}
+	var cfg = map[string]string{}
+	json.Unmarshal(cfg_bytes, &cfg)
+
+	client := http.Client{
+		Transport: &digest.Transport{
+			Username: cfg["Username"],
+			Password: cfg["Password"],
+		},
+	}
+	url := fmt.Sprintf("http://%s:%s/wsman", cfg["Host"], cfg["Port"])
+	wsman := new(WSMan)
+	n_uuid := uuid.New()
+	wsman_get_msg := wsman.CreateWsmanGet("AMT_GeneralSettings", n_uuid.String())
+	resp, err := client.Post(url, "text/xml", bytes.NewBuffer([]byte(wsman_get_msg)))
+	if err != nil {
+		t.Logf("Error: %s\n", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	ws, err := wsman.ParseWsman(bytes.NewBuffer([]byte(body)))
+	new_body := ws.Body
+	new_gs := new_body["AMT_GeneralSettings"]
+	new_gs.(map[string]interface{})["HostName"] = "NUCMC"
+	new_body["AMT_GeneralSettings"] = new_gs
+	str := createPutStringFromBody("AMT_GeneralSettings", new_body)
+	n_uuid = uuid.New()
+	wsman_put_msg := wsman.CreateWsmanPut("AMT_GeneralSettings", n_uuid.String(), "", str)
+	t.Logf("%s\n", wsman_put_msg)
+	resp, err = client.Post(url, "text/xml", bytes.NewBuffer([]byte(wsman_put_msg)))
+	if err != nil {
+		t.Logf("Error: %s\n", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	ws, err = wsman.ParseWsman(bytes.NewBuffer([]byte(body)))
+	js, _ := json.MarshalIndent(ws.Body, "", "   ")
+	t.Logf("%s\n", string(js))
 }
